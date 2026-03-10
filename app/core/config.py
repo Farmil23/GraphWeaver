dfe1774e545ee0effbe9be8f19e7fa7793e8e5fd
Farhan kamil Hermansyah <affschannel@gmail.com>
2026-03-10 16:55:38 +0700

    VERSION 1.0: UPDATE NEO4J DB

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "GraphWeaver"
-    NEO4J_URI: str = "bolt://localhost:7687"
+    NEO4J_URI: str = "bolt://neo4j:7687"
     NEO4J_USER: str = "neo4j"
     NEO4J_PASSWORD: str 
     GROQ_API_KEY: str 
